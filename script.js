bda240b775e3bdcb34c7d535108371a0b85518fe
Nergiz Şengün <nergizduygu56@gmail.com>
2025-01-28 14:05:36 +0300

    finish

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const word_el = document.getElementById('word');
 const popup = document.getElementById('popup-container');
 const message_el = document.getElementById('success-message');
+const wrongLetters_el = document.getElementById('wrong-letters');
+const items = document.querySelectorAll('.item');
+const message = document.getElementById('message');
+const PlayAgainBtn = document.getElementById('play-again');
 
-const correctLetters = ['j','a','v','s','t'];
-const wrongLetters = [];
+const correctLetters = []; // doğru harfler
+const wrongLetters = []; // yanlış harflerimiz
+let selectedWord = getRandomWord(); // global düzeyde olsun.
 
 
 function getRandomWord() {
-    const words = ["javascript","java","python"];
+    const words = ["javascript","java","python","html","css","php","mongo"];
     return words[Math.floor(Math.random() * words.length)];
 }
 
-function displayWord() {
-    const selectedWord = getRandomWord();
+function displayWord() { 
 
     word_el.innerHTML = `
         ${selectedWord.split('').map(letter => `
@@ -30,10 +34,68 @@ function displayWord() {
     }
 }
 
+function displayMessage(){
+    message.classList.add('show');
+
+    setTimeout(function(){
+        message.classList.remove('show');
+    },2000);
+}
+
+PlayAgainBtn.addEventListener('click', function(){
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = getRandomWord();
+    displayWord();
+    updateWrongLetters();
+
+    popup.style.display = 'none';
+})
+
+function updateWrongLetters(){
+    wrongLetters_el.innerHTML = `
+        ${wrongLetters.length>0?'<h3>Hatalı Harfler</h3>' : ''}
+        ${wrongLetters.map(letter=> `<span>${letter}</span>`)}
+    `;
+
+    items.forEach((items, index) => { // items içinde dolaşıyor, 
+        const errorCount = wrongLetters.length; // Hatalı harflerimizin sayısını alıyoruz.
+
+        if (index<errorCount){  // Eğer item'in index'i hatalı harf sayısından küçükse,
+            items.style.display = 'block';
+        }else {
+            items.style.display = 'none';
+        }
+    })
+    if (wrongLetters.length ===items.length) {
+        popup.style.display = 'flex';
+        message_el.innerText = 'Malesef Kaybettiniz :(';
+    }
+}
+
+
 window.addEventListener('keydown', function(e){
     if (e.keyCode >= 65 && e.keyCode <= 90){ //kullanıcı klavyeden bir tuşa bastığında bir olay tetikleniyor,sadece harf tuşlarına basıldığında işlemi gerçekleştirmek için bir filtre uygular.
-        console.log(e.key);
-        console.log(e.keyCode);
+        const letter = e.key;
+
+        if (selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter);
+                displayWord();
+            }else {
+                //console.log('bu harfi zaten eklediniz.');
+                displayMessage();
+            }
+        }else {
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+               // console.log('hatalı harfleri güncelle.'); 
+               updateWrongLetters();
+            }else{
+                displayMessage();
+            }
+        }
     }
 });
 
